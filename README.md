b8e99f7b3f2409a91ece6b772b12ecdc863d07ab
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-27 17:13:30 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# random-generators
+
+## Requirenments
+
+- `emacs 27.1` higher
+
+## Features
+
+- [x] Sequence generation by rules
+- [x] List generation by rules
+
+## Installation
+
+### Copy-paste plugin script contents to `~/.emacs` while enabling lexical-binding
+
+Example `~/.emacs` config:
+
+```emacs
+;;; -*- lexical-binding: t; -*-
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; The extension file content with all comments removed can be placed here.
+
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(package-selected-packages '(markdown-mode)))
+(custom-set-faces
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+)
+```
+
+## Functionality
+
+All `length` parameters are optional and defaults to `10`.
+By convention all parameters are ordered as follows: `selector`, and `length`.
+Below square brackets are used to represent optional parameters and **not** arrays.
+
+- Function: `generators-lazy-sequence selector [length]`
+  Description: Generate a sequence with a specific length.
+- Function: `generators-list selector [length]`
+  Description: Generate a list with a specific length.
+
+## FAQ
+
+- Why to use generators to generate sequences?
+  As long as sequences can be extremely long not to consume too much memory it's better to use generators.
---
+++ generators.el
@@ -0,0 +1,63 @@
+;;; generators.el --- Functions to generate data structures  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2022 Emily Grace Seville <EmilySeville7cfg@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;  For details view README file.
+
+;; URL: https://github.com/Emilyseville7cfg/generators
+
+;;; Code:
+
+(iter-defun generators-lazy-sequence(selector &optional length)
+  "Generate a sequence with a specific LENGTH.
+SELECTOR: the selector to project items (signature: selector(index))
+LENGTH: the sequence length (default: 10)"
+  (if (null selector)
+      (error "The selector '%s' is nil" selector))
+
+  (setq length (cond
+		((null length) 10)
+		(t length)))
+
+  (if (< length 0)
+      (error "The sequence length '%s' is less than zero" length))
+
+  (dotimes (i length)
+    (iter-yield (funcall selector i))))
+
+(defun generators-list(selector &optional length)
+  "Generate a list with a specific LENGTH.
+SELECTOR: the selector to project items (signature: selector(index))
+LENGTH: the list length (default: 10)"
+  (if (null selector)
+      (error "The selector '%s' is nil" selector))
+
+  (setq length (cond
+		((null length) 10)
+		(t length)))
+
+  (if (< length 0)
+      (error "The list length '%s' is less than zero" length))
+
+  (cl-loop for i upfrom 0 upto (1- length)
+	   collect (funcall selector i)))
+
+(provide 'generators)
+
+;;; generators.el ends here
